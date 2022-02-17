41f79c5c9922dfaffab01c20d93ce6e9f20431bb
rfritzURI <ryan_fritz@uri.edu>
2022-02-17 16:33:50 -0500

    Challenge Completed

---
+++ Coding_Challenge_2/Coding_Challenge_Part1.py
@@ -0,0 +1,9 @@
+# 1. List values
+# Using this list:
+#
+# [1, 2, 3, 6, 8, 12, 20, 32, 46, 85]
+# Make a new list that has all the elements less than 5 from this list in it and print out this new list.
+# Write this in one line of Python (you do not need to append to a list just print the output).
+# You should have two solutions in this file, one for item 1 and one for item 2. Item 2 is tricky so if you get stuck
+# try your best (no penalty), for a hint check out the solution by desiato here.
+
