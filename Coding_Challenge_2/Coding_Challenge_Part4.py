a07329bb4a80ea5547921138654d8c1c0c6e2a87
rfritzURI <ryan_fritz@uri.edu>
2022-02-18 11:04:24 -0500

    hw

--- Coding_Challenge_2/Coding_Challenge_Part4.py
+++ Coding_Challenge_2/Coding_Challenge_Part4.py
@@ -1,5 +1,13 @@
-# Given a singe phrase, count the occurrence of each word
-# Using this string:
+# 4. User input
+# Ask the user for an input of their current age, and tell them how
+# many years until they reach retirement (65 years old).
+#
+# Hint
 
 age = input("What is your age? ")
-print "Your age is " + str(age)
+print("Your age is " + str(26))
+
+name = input("What is your name: ")
+age = int(input("How old are you: "))
+year = str((2022 - 26)+65)
+print(name + " will be 100 years old in the year " + year)
---
+++ Coding_Challenge_2/Coding_Challenge_Part5.py
@@ -0,0 +1,16 @@
+# 5. User input 2
+# Using the following dictionary (or a similar one you found on the internet), ask the user for a word,
+# and compute the Scrabble word score for that word (Scrabble is a word game, where players make words
+# from letters, each letter is worth a point value), steal this code from the internet,
+# format it and make it work:
+
+
+letter_scores = {
+    "aeioulnrst": 1,
+    "dg": 2,
+    "bcmp": 3,
+    "fhvwy": 4,
+    "k": 5,
+    "jx": 8,
+    "qz": 10
+}
